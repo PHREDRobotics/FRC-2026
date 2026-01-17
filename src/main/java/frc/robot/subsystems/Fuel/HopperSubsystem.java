37df3ff9197ccd3034c230c2c9db83083b81ab3c
Nate <nateh3536@gmail.com>
2026-01-17 10:21:57 -0800

    Name is fixed.

--- src/main/java/frc/robot/subsystems/Fuel/HopperSubsystem.java
+++ src/main/java/frc/robot/subsystems/Fuel/HopperSubsystem.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems.Fuel;
 
 import frc.robot.Configs;
+import frc.robot.Configs.HopperSparkMax;
 import frc.robot.Constants;
 import frc.robot.Constants.FuelConstants;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -13,21 +14,21 @@ import com.revrobotics.spark.SparkLowLevel.MotorType;
 public class HopperSubsystem {
 
 
-  public SparkMax hopperMotorSparkMax = new SparkMax(FuelConstants.kHopperMotorCANId, MotorType.kBrushless);
+  public SparkMax hopperSparkMax = new SparkMax(FuelConstants.kHopperMotorCANId, MotorType.kBrushless);
 
 
 public HopperSubsystem() {
-    hopperMotorSparkMax.configure(Configs.HopperMotor.motorConfig, ResetMode.kResetSafeParameters,
+    hopperSparkMax.configure(Configs.HopperSparkMax.motorConfig, ResetMode.kResetSafeParameters,
         PersistMode.kPersistParameters);
   }
 
     public void stop() {
-    hopperMotorSparkMax.set(0);
+    hopperSparkMax.set(0);
   }
 
 
-  public void FeedHopper() {
-    hopperMotorSparkMax.set(FuelConstants.kHopperIntakeSpeed);
+  public void extendHopper() {
+    hopperSparkMax.set(FuelConstants.kHopperSpeed);
   }
 
 }
