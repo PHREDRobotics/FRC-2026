50bf1c3d13dcbe4f4ee05e8e88afcf403ac05237
Andy Glover <andrew@hillsideghost.com>
2026-02-07 16:06:56 -0800

    warnung remove

--- src/main/java/frc/robot/subsystems/intakeArm/IntakeArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/intakeArm/IntakeArmSubsystem.java
@@ -3,11 +3,9 @@ package frc.robot.subsystems.intakeArm;
 import com.revrobotics.PersistMode;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.ResetMode;
-import com.revrobotics.spark.FeedbackSensor;
 import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.SparkMax;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
